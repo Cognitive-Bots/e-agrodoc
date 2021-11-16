b471d45310d960504e9635a2658bdc5e5b7c2980
saishreddyk <saishreddy05@gmail.com>
2021-11-16 19:07:27 +0530

    Updated app.js and homescreen to bare minimum

--- screens/homescreen.js
+++ screens/homescreen.js
@@ -1,45 +1,105 @@
-import React from 'react';
+// import React from 'react';
+// import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <TouchableOpacity style={styles.button} 
+//         onPress={()=>{alert("Crop")}}>
+//           <Image source={{ uri: ""}}
+//           style={{
+//                     height: 300,
+//                     width: 350,
+//                     borderColor: "gray",
+//                     // borderWidth: 250
+//                 }}/>
+//         </TouchableOpacity>
+//         <TouchableOpacity style={styles.button} 
+//         onPress={()=>{alert("Fertilizer")}}>
+//           <Image source={{ uri: ""}}
+//           style={{
+//                     height: 300,
+//                     width: 350,
+//                     borderColor: "gray",
+//                     // borderWidth: 250
+//                 }}/>
+//         </TouchableOpacity>
+
+//         <TouchableOpacity style={styles.button} 
+//         onPress={()=>{alert("Disease")}}>
+//           <Image source={{ uri: ""}}
+//           style={{
+//                     height: 300,
+//                     width: 350,
+//                     borderColor: "gray",
+//                     // borderWidth: 250
+//                 }}/>
+//         </TouchableOpacity>
+
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#000000',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   button: {
+//     backgroundColor: '#859a9b',
+//     borderRadius: 10,
+//     // padding: 10,
+//     marginBottom: 20,
+//     // shadowColor: '#303838',
+//     // shadowOffset: { width: 10, height: 10},
+//     shadowRadius: 10,
+//     shadowOpacity: 0.35,
+//   },
+// });
+import React from "react";
+import { render } from "react-dom";
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button} 
-        onPress={()=>{alert("Crop")}}>
-          <Image source={{ uri: ""}}
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button}
+        onPress={() => { alert("Crop") }}>
+        <Image source={{ uri: "" }}
           style={{
-                    height: 300,
-                    width: 350,
-                    borderColor: "gray",
-                    // borderWidth: 250
-                }}/>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} 
-        onPress={()=>{alert("Fertilizer")}}>
-          <Image source={{ uri: ""}}
+            height: 300,
+            width: 350,
+            borderColor: "gray",
+            // borderWidth: 250
+          }} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}
+        onPress={() => { alert("Fertilizer") }}>
+        <Image source={{ uri: "" }}
           style={{
-                    height: 300,
-                    width: 350,
-                    borderColor: "gray",
-                    // borderWidth: 250
-                }}/>
-        </TouchableOpacity>
+            height: 300,
+            width: 350,
+            borderColor: "gray",
+            // borderWidth: 250
+          }} />
+      </TouchableOpacity>
 
-        <TouchableOpacity style={styles.button} 
-        onPress={()=>{alert("Disease")}}>
-          <Image source={{ uri: ""}}
+      <TouchableOpacity style={styles.button}
+        onPress={() => { alert("Disease") }}>
+        <Image source={{ uri: "" }}
           style={{
-                    height: 300,
-                    width: 350,
-                    borderColor: "gray",
-                    // borderWidth: 250
-                }}/>
-        </TouchableOpacity>
-        
-      </View>
-    );
-  }
+            height: 300,
+            width: 350,
+            borderColor: "gray",
+            // borderWidth: 250
+          }} />
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -60,3 +120,5 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.35,
   },
 });
+
+export default HomeScreen ;
