27b5f39c4b077a084fd9ed5b96308e0b033f4570
saishreddyk <saishreddy05@gmail.com>
2021-11-28 17:30:28 +0530

    Removed unnecessary dependencies

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import HomeStack from './routes/homeStack';
 import NavContainer from './routes/drawer';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 export default function App() {
   return (
     <NavContainer />
@@ -12,7 +10,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: '#fff',
     backgroundColor: '#FF5236',
   },
 });
