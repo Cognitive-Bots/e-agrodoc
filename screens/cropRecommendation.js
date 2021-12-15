0f3591c39646897d24cd7f79eed466adba2ba5b7
shashank822 <65481401+shashank822@users.noreply.github.com>
2021-12-15 23:05:37 +0530

    Update cropRecommendation.js

--- screens/cropRecommendation.js
+++ screens/cropRecommendation.js
@@ -1,43 +1,47 @@
 import * as React from 'react';
-import 'cities.js';
 import {Text, StyleSheet, View, TextInput, Button} from 'react-native';
-// import Constants from 'expo-constants';
+import Constants from 'expo-constants';
 
 // You can import from local files
-// import AssetExample from './components/AssetExample';
+import AssetExample from './components/AssetExample';
 
-// // or any pure javascript modules available in npm
-// import { Card } from 'react-native-paper';
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
 
-export default function CropRecommendation() {
-  const [count,setCount]=([0,0,0,0,0]);
-  const [state,setState]=("");
+export default function App() {
+  const [count1,setCount1]=[0];
+  const [count2,setCount2]=[0];
+  const [count3,setCount3]=[0];
+  const [count4,setCount4]=[0];
+  const [count5,setCount5]=[0];
+  const [count6,setCount6]=[0];
   return (
-    <View >
+    <View style={{flex:1,justifyContent:"center",backgroundColor:"#fff", alignItems:"center"}}>
+    <View style={{width:200,height:500,backgroundColor:"yellow",padding:50}}>
+     <View >
       <Text >Find out the most suitable crop to grow in your farm</Text>
       <View>
         <Text>Nitrogen</Text>
         <TextInput 
-          placeholder="Enter the value(Example:50)"  onChange={(e)=>setCount(e[0].target.value)}/>
+          placeholder="Enter the value(Example:50)"  onChange={(count1)=>setCount1(count1)}/>
         <Text>Phosporous</Text>
         <TextInput
-          placeholder="Enter the value(Example:50)"  onChange={(e)=>setCount(e[1].target.value)}
+          placeholder="Enter the value(Example:50)"  onChange={(count2)=>setCount2(count2)}
         />
         <Text>Potassium</Text>
         <TextInput 
-          placeholder="Enter the value(Example:50)"  onChange={(e)=>setCount(e[2].target.value)}/>
+          placeholder="Enter the value(Example:50)"  onChange={(count3)=>setCount3(count3)}/>
         <Text>ph level</Text>
         <TextInput
-          placeholder="Enter the value"  onChange={(e)=>setCount(e[3].target.value)}
+          placeholder="Enter the value"  onChange={(count4)=>setCount4(count4)}
         />
         <Text>Rainfall(in mm)</Text>
         <TextInput 
-          placeholder="Enter the value"  onChange={(e)=>setCount(e[4].target.value)}/>
-        <Text>State</Text>
-        <select onchange="print_city('state', this.selectedIndex);" id="sts" name ="stt" class="form-control" required></select>
-        <select id ="state" class="form-control" required></select>
-        <script language="javascript">print_state("sts");</script>
+          placeholder="Enter the value"  onChange={(count5)=>setCount5(count5)}/>
         <button value="Predict">Predict</button>
+        <button onSubmit={console.log(count1,count2,count3,count4,count5)}>Submit</button>
+      </View>
+    </View>
       </View>
     </View>
 )};
