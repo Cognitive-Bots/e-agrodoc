9c6f50e074adc735defff2a2a9d8b037b5fa72e1
shashank822 <65481401+shashank822@users.noreply.github.com>
2021-12-15 22:58:32 +0530

    Update fertilizerRecommendation.js

--- screens/fertilizerRecommendation.js
+++ screens/fertilizerRecommendation.js
@@ -1,26 +1,38 @@
 import * as React from 'react';
 import {Text, StyleSheet, View, TextInput, Button} from 'react-native';
 import Constants from 'expo-constants';
-const { useState } = React;
-const fertilizerRecommendation = ({navigation}) => {
-    const [count,setCount]=([0,0,0,0,0]);
-  const [crop,setCrop]=("");
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default function App() {
+  const [count1,setCount1]=[0];
+  const [count2,setCount2]=[0];
+  const [count3,setCount3]=[0];
+  const [count4,setCount4]=[0];
+  const [count5,setCount5]=[0];
+  const [count6,setCount6]=[0];
   return (
+    <View style={{flex:1,justifyContent:"center",backgroundColor:"#fff", alignItems:"center"}}>
+    <View style={{width:200,height:500,backgroundColor:"yellow",padding:50}}>
     <View >
       <Text >Get informed advice on fertilizer based on soil</Text>
       <View>
         <Text>Nitrogen</Text>
         <TextInput
-          placeholder="Enter the value(Example:50)" onChange={(e)=>setCount(e[0].target.value)} required/>
+          placeholder="Enter the value(Example:50)" onChange={count1=>setCount1(count1)} required/>
         <Text>Phosporous</Text>
-        <TextInput placeholder="Enter the value(Example:50)" onChange={(e)=>setCount(e[1].target.value)} required/>  
+        <TextInput placeholder="Enter the value(Example:50)" onChange={count2=>setCount2(count2)} required/>  
         <Text>Potassium</Text>
-        <TextInput  placeholder="Enter the value(Example:50)" onChange={(e)=>setCount(e[2].target.value)} required/>
+        <TextInput  placeholder="Enter the value(Example:50)" onChange={count3=>setCount3(count3)} required/>
         <Text>ph level</Text>
-        <TextInput placeholder="Enter the value" onChange={(e)=>setCount(e[3].target.value)} required/>
+        <TextInput placeholder="Enter the value" onChange={count4=>setCount4(count4)} required/>
         <Text>Rainfall(in mm)</Text>
-        <TextInput placeholder="Enter the value" onChange={(e)=>setCount(e[4].target.value)} required/>
-        <select name ="Crop" id ="Crop" placeholder="Crop" onChange={(e)=>setCrop(e.target.value)}>
+        <TextInput placeholder="Enter the value" onChange={count5=>setCount5(count5)} required/>
+        <select name ="Crop" id ="Crop" placeholder="Crop" >
           <option value="" disabled selected>Select Crop</option>
           <option value="rice">rice</option>
           <option value="maize">Maize</option>
@@ -46,9 +58,9 @@ const fertilizerRecommendation = ({navigation}) => {
           <option value="coffee">coffee</option>
         </select>
         <input type="submit" value="Predict" />
-        
+        <button onclick={()=>console.log({count1,count2,count3,count4,count5})}>Print</button>
+      </View>
+    </View>
       </View>
     </View>
 )};
-
-export default fertilizerRecommendation;
