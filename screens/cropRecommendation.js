6cfce07b95f43ff668a56be679287adca64dfb97
saishreddyk <saishreddy05@gmail.com>
2021-12-16 18:43:45 +0530

    Added code of cropRecom, fertilizerRecom to dev_drawer

--- screens/cropRecommendation.js
+++ screens/cropRecommendation.js
@@ -1,76 +1,41 @@
-import * as React from 'react';
-import {Text, StyleSheet, View, TextInput, Button} from 'react-native';
-// import Constants from 'expo-constants';
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, TextInput, Button } from 'react-native';
 
-// You can import from local files
-// import AssetExample from './components/AssetExample';
-
-// // or any pure javascript modules available in npm
-// import { Card } from 'react-native-paper';
 
 export default function CropRecommendation() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
+  const [count4, setCount4] = useState(0);
+  const [count5, setCount5] = useState(0);
+
   return (
-    <View >
-      <Text >Find out the most suitable crop to grow in your farm</Text>
-      <View>
-        <Text>Nitrogen</Text>
-        <TextInput 
-          placeholder="Enter the value(Example:50)" />
-        <Text>Phosporous</Text>
-        <TextInput
-          placeholder="Enter the value(Example:50)"
-        />
-        <Text>Potassium</Text>
-        <TextInput 
-          placeholder="Enter the value(Example:50)" />
-        <Text>ph level</Text>
-        <TextInput
-          placeholder="Enter the value"
-        />
-        <Text>Rainfall(in mm)</Text>
-        <TextInput 
-          placeholder="Enter the value" />
-        <Text>State</Text>
-        <select name ="state" id ="state" placeholder="state">
-          <option value="" disabled selected>Select State</option>
-          <option value="Andhra Pradesh">Andhra Pradesh</option>
-          <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
-          <option value="Arunachal Pradesh">Arunachal Pradesh</option>
-          <option value="Assam">Assam</option>
-          <option value="Bihar">Bihar</option>
-          <option value="Chandigarh">Chandigarh</option>
-          <option value="Chhattisgarh">Chhattisgarh</option>
-          <option value="Dadar and Nagar Haveli">Dadar and Nagar Haveli</option>
-          <option value="Daman and Diu">Daman and Diu</option>
-          <option value="Delhi">Delhi</option>
-          <option value="Lakshadweep">Lakshadweep</option>
-          <option value="Puducherry">Puducherry</option>
-          <option value="Goa">Goa</option>
-          <option value="Gujarat">Gujarat</option>
-          <option value="Haryana">Haryana</option>
-          <option value="Himachal Pradesh">Himachal Pradesh</option>
-          <option value="Jammu and Kashmir">Jammu and Kashmir</option>
-          <option value="Jharkhand">Jharkhand</option>
-          <option value="Karnataka">Karnataka</option>
-          <option value="Kerala">Kerala</option>
-          <option value="Madhya Pradesh">Madhya Pradesh</option>
-          <option value="Maharashtra">Maharashtra</option>
-          <option value="Manipur">Manipur</option>
-          <option value="Meghalaya">Meghalaya</option>
-          <option value="Mizoram">Mizoram</option>
-          <option value="Nagaland">Nagaland</option>
-          <option value="Odisha">Odisha</option>
-          <option value="Punjab">Punjab</option>
-          <option value="Rajasthan">Rajasthan</option>
-          <option value="Sikkim">Sikkim</option>
-          <option value="Tamil Nadu">Tamil Nadu</option>
-          <option value="Telangana">Telangana</option>
-          <option value="Tripura">Tripura</option>
-          <option value="Uttar Pradesh">Uttar Pradesh</option>
-          <option value="Uttarakhand">Uttarakhand</option>
-          <option value="West Bengal">West Bengal</option>
-        </select>
-        <button value="Predict">Predict</button>
+    <View style={{ flex: 1, justifyContent: "center", backgroundColor: "#fff", alignItems: "center" }}>
+      <View style={{ width: 200, height: 500, backgroundColor: "yellow", padding: 50 }}>
+        <View >
+          <Text >Find out the most suitable crop to grow in your farm</Text>
+          <View>
+            <Text>Nitrogen</Text>
+            <TextInput
+              placeholder="Enter the value(Example:50)" onChange={(e) => setCount1(e.target.value)} />
+            <Text>Phosporous</Text>
+            <TextInput
+              placeholder="Enter the value(Example:50)" onChange={(e) => setCount2(count2)}
+            />
+            <Text>Potassium</Text>
+            <TextInput
+              placeholder="Enter the value(Example:50)" onChange={(count3) => setCount3(count3)} />
+            <Text>ph level</Text>
+            <TextInput
+              placeholder="Enter the value" onChange={(count4) => setCount4(count4)}
+            />
+            <Text>Rainfall(in mm)</Text>
+            <TextInput
+              placeholder="Enter the value" onChange={(count5) => setCount5(count5)} />
+            <Button title="Submit" onPress={() => console.log(count1)} />
+          </View>
+        </View>
       </View>
     </View>
-)};
+  )
+};
--- screens/fertilizerRecommendation.js
+++ screens/fertilizerRecommendation.js
@@ -1,11 +1,58 @@
-import { Text, View } from 'react-native';
-import React from "react";
-const fertilizerRecommendation = ({navigation}) => {
-    return(
-        <View>
-            <Text>fertilizerRecommendation</Text>
-        </View>
-    )
-}
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, TextInput, Button } from 'react-native';
 
-export default fertilizerRecommendation;
+export default function FertilizerRecommendation() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
+  const [count4, setCount4] = useState(0);
+  const [count5, setCount5] = useState(0);
+  return (
+    <View style={{ flex: 1, justifyContent: "center", backgroundColor: "#fff", alignItems: "center" }}>
+      <View style={{ width: 200, height: 500, backgroundColor: "yellow", padding: 50 }}>
+        <View >
+          <Text >Get informed advice on fertilizer based on soil</Text>
+          <View>
+            <Text>Nitrogen</Text>
+            <TextInput
+              placeholder="Enter the value(Example:50)" onChange={(e) => setCount1(e.target.value())} required />
+            <Text>Phosporous</Text>
+            <TextInput placeholder="Enter the value(Example:50)" onChange={count2 => setCount2(count2)} required />
+            <Text>Potassium</Text>
+            <TextInput placeholder="Enter the value(Example:50)" onChange={count3 => setCount3(count3)} required />
+            <Text>ph level</Text>
+            <TextInput placeholder="Enter the value" onChange={count4 => setCount4(count4)} required />
+            <Text>Rainfall(in mm)</Text>
+            <TextInput placeholder="Enter the value" onChange={count5 => setCount5(count5)} required />
+            <select name="Crop" id="Crop" placeholder="Crop" >
+              <option value="" disabled selected>Select Crop</option>
+              <option value="rice">rice</option>
+              <option value="maize">Maize</option>
+              <option value="chickpea">chickpea</option>
+              <option value="kidneybeans">kidneybeans</option>
+              <option value="pigeonpeas">pigeonpeas</option>
+              <option value="mothbeans">mothbeans</option>
+              <option value="mungbeans">mungbeans</option>
+              <option value="blackgram">blackgram</option>
+              <option value="lentil">lentil</option>
+              <option value="banana">banana</option>
+              <option value="pomegranate">pomegranate</option>
+              <option value="mango">mango</option>
+              <option value="grapes">grapes</option>
+              <option value="watermelon">watermelon</option>
+              <option value="muskmelon">muskmelon</option>
+              <option value="apple">apple</option>
+              <option value="orange">orange</option>
+              <option value="papaya">papaya</option>
+              <option value="coconut">coconut</option>
+              <option value="cotton">cotton</option>
+              <option value="jute">jute</option>
+              <option value="coffee">coffee</option>
+            </select>
+            <Button title="Submit" onPress={() => console.log(count1)} />
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+};
