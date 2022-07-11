7215d55adecac2967552b41f62c95d7d73a0a83d
shashank822 <shashankdvn@gmail.com>
2022-07-11 16:43:49 +0000

    added form

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const ip = "192.168.29.54"
+const ip = "192.168.1.105"
 const auth_port = 1111
 const upload_port = 8010
 const flask1_port = 8012
@@ -7,7 +7,7 @@ const config = {
     "auth_ip": `http://${ip}:${auth_port}/`,
     "upload_ip": `http://${ip}:${upload_port}/`,
     "flask1_ip": `http://${ip}:${flask1_port}/image`,
-    "flask2_ip": `http://${ip}:${flask2_port}/gps`,
+    "flask2_ip": `http://${ip}:${flask2_port}/`,
 }
 
 export default config;
