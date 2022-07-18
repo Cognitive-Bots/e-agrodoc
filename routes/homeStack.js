d84c303e3b4541668c0d22dc733de220b10eecb5
saishreddyk <saishreddy05@gmail.com>
2022-07-18 17:26:29 +0000

    Refactored

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,38 +1,18 @@
 import * as React from "react";
 import "react-native-gesture-handler";
-// import { NavigationState } from 'react-navigation';
-
-// import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/homescreen";
 import DiseaseDetection from "../screens/disease_detection";
 import CropRecommendation from "../screens/crop_recommendation";
-
-// const getActiveRouteState = function (route){
-//   if (!route.routes || route.routes.length === 0 || route.index >= route.routes.length) {
-//       return route;
-//   }
-
-//   const childActiveRoute = route.routes[route.index];
-//   return getActiveRouteState(childActiveRoute);
-// }
 
 const HomeStack = (props) => {
   const Stack = createStackNavigator();
   return (
     <Stack.Navigator
       initialRouteName="Home"
-      // show header only if in Home Screen
-      screenOptions={
-        ({ route }) => ({
-          headerShown: route.name != "Home",
-          headerTitle: route.name === "Home" ? "E-AgroDoc" : "",
-          headerStyle: {
-            backgroundColor: "#fdf6e3",
-            borderBottomWidth: 0,
-          },
-        })
-      }
+      screenOptions={{
+        headerShown: false
+      }}
     >
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="disease_detection" component={DiseaseDetection} />
